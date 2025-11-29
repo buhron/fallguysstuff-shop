006d4ecb7d6afbcaaa548b9356360f3cfbe4c5f9
buhron <mbh90180@gmail.com>
2025-11-29 17:52:13 -0500

    Add giftEnter function for gift code entry

---
+++ index.js
@@ -0,0 +1,5 @@
+function giftEnter() {
+  alert("Gift code entered! You will successfully be delivered new early FallGuysStuff merch before December 1.");
+  alert("Gift code entered! You will successfully be delivered new early FallGuysStuff merch before December 1.");
+  prompt("Btw what is your address? Weird but so I can not only deliver the merch, but also.. kiss you")
+}
